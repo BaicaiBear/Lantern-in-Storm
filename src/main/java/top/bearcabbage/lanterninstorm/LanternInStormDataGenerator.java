2a731c6e53de64fe86a12b9deecd32f2d7479ef0
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2024-12-13 20:27:47 +0800

    Initial commit

---
+++ src/main/java/top/bearcabbage/lanterninstorm/LanternInStormDataGenerator.java
@@ -0,0 +1,11 @@
+package top.bearcabbage.lanterninstorm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LanternInStormDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
