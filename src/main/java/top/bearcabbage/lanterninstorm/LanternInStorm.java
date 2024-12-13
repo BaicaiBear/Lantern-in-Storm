67895429ff17ad42cc7907d4dbfe432638a05259
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2024-12-13 21:18:10 +0800

    注册了命令

--- src/main/java/top/bearcabbage/lanterninstorm/LanternInStorm.java
+++ src/main/java/top/bearcabbage/lanterninstorm/LanternInStorm.java
@@ -4,6 +4,7 @@ import eu.pb4.playerdata.api.PlayerDataApi;
 import eu.pb4.playerdata.api.storage.NbtDataStorage;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,5 +18,8 @@ public class LanternInStorm implements ModInitializer {
 
 		// 获取配置文件
 		PlayerDataApi.register(LSData);
+		// 使用CommandRegistrationCallback.EVENT注册命令
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment)->LanternInStormCommand.registerCommands(dispatcher)); // 调用静态方法注册命令
+
 	}
 }
