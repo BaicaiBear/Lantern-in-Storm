6e2fdb74ae7328b257fe8fcaab14e2f0149fd9bb
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2024-12-16 23:04:01 +0800

    规范化了代码

--- src/main/java/top/bearcabbage/lanterninstorm/network/DistributingSpiritsPayloadHandler.java
+++ src/main/java/top/bearcabbage/lanterninstorm/network/DistributingSpiritsPayloadHandler.java
@@ -3,16 +3,22 @@ package top.bearcabbage.lanterninstorm.network;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
+import top.bearcabbage.lanterninstorm.LanternInStormSpiritManager;
 import top.bearcabbage.lanterninstorm.entity.SpiritLanternEntity;
 
 
 public class DistributingSpiritsPayloadHandler {
 
+    // 收到加减灵魂的包
+    public static void onDistributingSpiritsPayload(DistributingSpiritsPayload payload, ServerPlayNetworking.Context context)  {
+            LanternInStormSpiritManager.set(context.player().getUuid(), payload.lantern(), payload.spirits());
+    }
 
 }
