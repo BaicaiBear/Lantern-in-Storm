4bd8aeaf0f8d9f10a80fc7cc05dd6d4ca3b54c0d
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-01-13 21:31:19 +0800

    修复了灯笼的爆炸问题

--- src/main/java/top/bearcabbage/lanterninstorm/lantern/SpiritLanternBlock.java
+++ src/main/java/top/bearcabbage/lanterninstorm/lantern/SpiritLanternBlock.java
@@ -12,8 +12,10 @@ import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 import top.bearcabbage.lanterninstorm.particle.BorderParticle;
 import top.bearcabbage.lanterninstorm.player.LiSPlayer;
 import top.bearcabbage.lanterninstorm.player.Player;
@@ -27,6 +29,11 @@ public class SpiritLanternBlock extends LanternBlock implements Waterloggable {
     public SpiritLanternBlock(Settings settings) {
         super(settings);
         this.setDefaultState(this.stateManager.getDefaultState().with(STARTUP, false));
+    }
+
+    @Override
+    protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return true;
     }
 
     @Override
--- src/main/java/top/bearcabbage/lanterninstorm/lantern/SpiritLanternBlocks.java
+++ src/main/java/top/bearcabbage/lanterninstorm/lantern/SpiritLanternBlocks.java
@@ -4,8 +4,6 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.piston.PistonBehavior;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -16,7 +14,7 @@ import static top.bearcabbage.lanterninstorm.LanternInStorm.MOD_ID;
 
 public abstract class SpiritLanternBlocks {
     public static final Block WHITE_PAPER_LANTERN = (Block) Registry.register(Registries.BLOCK, Identifier.of(MOD_ID,"white_paper_lantern"), new SpiritLanternBlock(AbstractBlock.Settings.create().mapColor(MapColor.WHITE).requiresTool().strength(10.0F, 3600000.0F)/*2x铁门硬度基岩爆炸抗性*/.sounds(BlockSoundGroup.LANTERN).luminance((state) -> {return 10;}).nonOpaque().pistonBehavior(PistonBehavior.BLOCK)));
-    public static final Block OAK_WOODEN_LANTERN = (Block) Registry.register(Registries.BLOCK, Identifier.of(MOD_ID,"oak_wooden_lantern"), new SpiritLanternBlock(AbstractBlock.Settings.create().mapColor(MapColor.BROWN).requiresTool().strength(10.0F, 3600000.0F).sounds(BlockSoundGroup.LANTERN).luminance((state) -> {return 10;}).nonOpaque().pistonBehavior(PistonBehavior.BLOCK)));
+    public static final Block OAK_WOODEN_LANTERN = (Block) Registry.register(Registries.BLOCK, Identifier.of(MOD_ID,"oak_wooden_lantern"), new SpiritLanternBlock(AbstractBlock.Settings.create().mapColor(MapColor.BROWN).requiresTool().strength(10.0F, 3600000.0F).sounds(BlockSoundGroup.WOOD).luminance((state) -> {return 10;}).nonOpaque().pistonBehavior(PistonBehavior.BLOCK)));
 
     public static void initialize(){}
 }
